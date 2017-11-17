29189c301bb6930b5a233e0f369868ae2f20c2e3
montygoldy <montygoldy2006@gmail.com>
2017-11-17 04:48:08 +0000

    Added mongodb as database

--- server/index.js
+++ server/index.js
@@ -11,8 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  // If there's a connection error, log it and throw error
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -28,6 +35,7 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
